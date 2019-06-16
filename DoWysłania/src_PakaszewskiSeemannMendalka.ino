8e967cf430b93ff060f12eae62e755cbf6ac6599
KrzysztofPakaszewski <krzysztof.pakaszewski@gmail.com>
2019-06-16 12:20:00 +0200

    podmiana skryptu

--- DoWysłania/src_PakaszewskiSeemannMendalka.ino
+++ DoWysłania/src_PakaszewskiSeemannMendalka.ino
@@ -6,13 +6,12 @@ const int data = 8;
 const int trigPin = 12;
 const int echoPin = 11;
 
-//.GFE DCBA
-//0110 1101
-int digits[]= {0x3f,0x06,0x5b,0x4f,0x66,0x6d,
+int digits[] = {0x3f,0x06,0x5b,0x4f,0x66,0x6d,
     0x7d,0x07,0x7f,0x6f};
 int dist;
-bool test =true;
-bool showValue= false;
+bool test = true;
+bool showValue = false;
+
 void sendCommand(uint8_t value)
 {
   digitalWrite(strobe, LOW);
@@ -50,7 +49,7 @@ uint8_t readButtons(void)
   uint8_t buttons = 0;
   digitalWrite(strobe, LOW);
   shiftOut(data, clock, LSBFIRST, 0x42);
-
+  
   pinMode(data, INPUT);
 
   for (uint8_t i = 0; i < 4; i++)
@@ -61,6 +60,7 @@ uint8_t readButtons(void)
 
   pinMode(data, OUTPUT);
   digitalWrite(strobe, HIGH);
+  
   return buttons;
 }
 
@@ -75,8 +75,8 @@ void setLed(uint8_t value, uint8_t position)
   digitalWrite(strobe, HIGH);
 }
 
-// 0100 0000
-double measure(){
+double measure()
+{
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   // Sets the trigPin on HIGH state for 10 micro seconds
@@ -86,72 +86,78 @@ double measure(){
   // Reads the echoPin, returns the sound wave travel time in microseconds
   long duration = pulseIn(echoPin, HIGH);
   // Calculating the distance
-  double distance= duration*0.034/2;
+  double distance= duration * 0.034/2;
   return distance;
+}
+  
+double distance()
+{
+  double sum = 0;
+  
+  for(int a = 0; a < 50; a++)
+  {
+    sum += measure();
   }
-double distance(){
-  double sum =0;
-  for(int a =0; a<50; a++){
-      sum+=measure();
-  }
-    return sum/50;
+  
+  return sum/50;
 }
 
 
 void showDistance(int dist){
-    int l = floor(log10(dist));
   
-    for(uint8_t position = 8-l; position < 8; position++)
-    {
-      int wyswietl = dist/pow(10,8-position);
-      dist = dist % (int)(pow(10,8-position));
+  int l = floor(log10(dist));
+  
+  for(uint8_t position = 8 - l; position < 8; position++)
+  {
+    int wyswietl = dist/pow(10, 8 - position);
+    dist = dist % (int)(pow(10, 8 - position));
       
-      uint8_t maska =digits[wyswietl];
+    uint8_t maska = digits[wyswietl];
   
-      if (position == 6)
-      {
-        maska = maska | 0x80;
-      }
+    if (position == 6)
+    {
+      maska = maska | 0x80;
+    }
   
-      setLed(maska, position);
-      }
+    setLed(maska, position);
+  }
 }
 
-void defaultScreen(){
+void defaultScreen()
+{
   for(uint8_t position = 0; position < 8; position++)
-    {
-      setLed(0x40, position);
-     }
+  {
+    setLed(0x40, position);
   }
+}
 
 void loop()
 {
- //reset();
- uint8_t buttons = readButtons();
+  uint8_t buttons = readButtons();
+  uint8_t button1 = buttons & 0x01;
+  uint8_t button2 = buttons & 0x02;
 
- uint8_t button1 = buttons & 0x01;
- uint8_t button2 = buttons & 0x02;
-
- 
- if(button1 && test){
+  if(button1 && test){
     reset();
     showValue = true;
     dist = (int)(distance()*100);
     test= false;
   } 
+  
   if(!button1){
     test= true;
-    }
-
-   if(button2){
-      showValue= false;
-    }
-   if(showValue){
-     showDistance(dist);
-   }
-   else{
-        defaultScreen();
-        delayMicroseconds(200);
-        reset();
-   }
+  }
+  
+  if(button2){
+    showValue= false;
+  }
+  
+  if(showValue){
+    showDistance(dist);
+  }
+  else{
+    defaultScreen();
+    delayMicroseconds(200);
+    reset();
+  }
 }
